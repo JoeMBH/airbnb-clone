4c82c6d65d5e7d23f375cb3795bea58de18401a4
JoeMBH <joembhu@gmail.com>
2023-06-11 17:53:52 +0300

    modal toggle

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,23 +1,24 @@
 "use client";
+
 import axios from "axios";
-
 import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
-
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import Modal from "./Modal";
-
-import Heading from "../Heading";
-import Input from "../inputs/Input";
-import { toast } from "react-hot-toast";
-import Button from "../Button";
-
 import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
+import Button from "../Button";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -38,19 +39,26 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success("Registered!");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
-        toast.error("Something went wrong...");
+        toast.error(error);
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
 
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal]);
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome to Airbnb!" subtitle="Create an account!" />
+      <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
       <Input
         id="email"
         label="Email"
@@ -69,8 +77,8 @@ const RegisterModal = () => {
       />
       <Input
         id="password"
-        type="password"
         label="Password"
+        type="password"
         disabled={isLoading}
         register={register}
         errors={errors}
@@ -96,25 +104,26 @@ const RegisterModal = () => {
       />
       <div
         className="
-          text-neutral-500
-          text-center      
-          mt-4
+          text-neutral-500 
+          text-center 
+          mt-4 
           font-light
         "
       >
-        <div className="justify-center flex flex-row items-center gap-3">
-          <div>Already have an account?</div>
-          <div
-            onClick={registerModal.onClose}
+        <p>
+          Already have an account?
+          <span
+            onClick={onToggle}
             className="
               text-neutral-800
-              cursor-pointer
+              cursor-pointer 
               hover:underline
             "
           >
+            {" "}
             Log in
-          </div>
-        </div>
+          </span>
+        </p>
       </div>
     </div>
   );
@@ -132,4 +141,5 @@ const RegisterModal = () => {
     />
   );
 };
+
 export default RegisterModal;
